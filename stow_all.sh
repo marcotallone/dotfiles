5d0589a37d5a1cf7703dc5d1737279b828da0804
Marco Tallone <marco.tallone@studenti.units.it>
2025-02-10 22:38:47 +0100

    Updating all dotfiles with stow configuration and new hyprland settings

---
+++ stow_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script to stow all the configuration files
+stow zsh
+stow vim
+stow tmux
+stow nvim
+stow alacritty
+stow i3
+stow polybar
+# stow rofi
+stow fastfetch
+stow p10k
+stow picom
+# stow dunst
