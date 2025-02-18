9c54881374a811502c4682c68baad51596255fd5
Marco Tallone <marco.tallone@studenti.units.it>
2025-02-18 15:58:38 +0100

    Updated monitor selection scripts and emoji shortcuts

---
+++ scripts/monitor.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Monitor mode switching script
+
+# Define the path to the monitor configuration file
+MONITOR_CONF=${MONITOR_CONF:-/home/marco/dotfiles/hypr/.config/hypr/monitor.conf}
+
+# Dual monitor configuration
+DUAL_CONFIG="# Dual Monitor Configuration
+monitor = eDP-1, highres, 1920x0, 1
+monitor = HDMI-A-1, highres, 0x0, 1
+
+# Associated workspaces
+workspace = 1, monitor:HDMI-A-1
+workspace = 2, monitor:HDMI-A-1
+workspace = 3, monitor:HDMI-A-1
+workspace = 4, monitor:HDMI-A-1
+workspace = 5, monitor:HDMI-A-1
+workspace = 6, monitor:eDP-1
+workspace = 7, monitor:eDP-1
+workspace = 8, monitor:eDP-1
+workspace = 9, monitor:eDP-1
+workspace = 10, monitor:eDP-1"
+
+# Primary monitor configuration
+PRIMARY_CONFIG="# Primary (Laptop) Monitor Configuration
+monitor = eDP-1, highres, auto, 1
+monitor = HDMI-A-1, disable"
+
+# Secondary monitor configuration
+SECONDARY_CONFIG="# Seconndary Monitor Configuration
+monitor = eDP-1, disable
+monitor = HDMI-A-1, highres, auto, 1"
+
+# Mirror monitor configuration
+MIRROR_CONFIG="# Mirror Monitor Configuration
+monitor = eDP-1, highres, auto, 1, mirror, HDMI-A-1
+monitor = HDMI-A-1, highres, auto, 1, mirror, eDP-1"
+
+# Check the argument passed to the script
+if [ "$1" == "dual" ]; then
+    echo "$DUAL_CONFIG" > "$MONITOR_CONF"
+elif [ "$1" == "primary" ]; then
+    echo "$PRIMARY_CONFIG" > "$MONITOR_CONF"
+elif [ "$1" == "secondary" ]; then
+		echo "$SECONDARY_CONFIG" > "$MONITOR_CONF"
+elif [ "$1" == "mirror" ]; then
+		echo "$MIRROR_CONFIG" > "$MONITOR_CONF"
+else
+    echo "Usage: $0 {dual|primary}"
+    exit 1
+fi
+
