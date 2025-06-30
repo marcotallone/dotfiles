7f2f879b5b7943d2409788612e1abe4a9d5372f4
Marco Tallone <marco.tallone@studenti.units.it>
2025-06-30 16:08:57 +0200

    Updated nvim configuration and tmux terminal clors options

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -7,5 +7,9 @@ return {
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
 		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		config = function()
+			require("render-markdown").setup({ html = { enabled = false } })
+			require("render-markdown").setup({ latex = { enabled = false } })
+		end,
 	},
 }
